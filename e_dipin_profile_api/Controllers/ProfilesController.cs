303561f39da29c5f7c553139e65933063c4f7d80
Freedom Khanyile <freedom.khanyile1@gmail.com>
2021-07-12 20:37:15 +0200

    Create microservice projects

---
+++ e_dipin_profile_api/Controllers/ProfilesController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using e_dipin_profile_api.Models;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace e_dipin_profile_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfilesController : ControllerBase
+    {
+        private readonly ILogger<ProfilesController> _logger;
+
+        public ProfilesController(ILogger<ProfilesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        { 
+            return Ok(GetProfiles());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id) {
+            var profiles = GetProfiles();
+            var item  = profiles.Find(x => x.Id == id);
+            return Ok(item);
+        }
+
+        private List<UserProfile> GetProfiles()
+        {
+            return new List<UserProfile>() {
+                new UserProfile()
+                {
+                    Id = "1",
+                    Username = "Busani Zulu",
+                    Cellphone = "+2114454214",
+                    Password = null,
+                    IsDeleted = false
+                }, 
+                new UserProfile()
+                {
+                    Id = "2",
+                    Username = "Velile Zondo",
+                    Cellphone = "+21145457845",
+                    Password = null,
+                    IsDeleted = false
+                }, 
+                new UserProfile()
+                {
+                    Id = "3",
+                    Username = "Bongani Zulu",
+                    Cellphone = "+21124548754",
+                    Password = null,
+                    IsDeleted = false
+                }
+            };
+        }
+
+    }
+}
